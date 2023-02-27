1ae32903c7ffeef2827106d8a0320a49cda24a00
Alex Gutierrez <algusaem@gmail.com>
2023-02-27 19:51:27 +0100

    AG - Get Listas

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Primer mensaje de la API');
 });
 
+app.get('/listas/' + id, (req, res) => {
+  res.send('Devuelve las listas de tareas con la id' + id);
+});
+
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
