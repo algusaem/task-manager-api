5cd1edd8b4584b472e33a67724f8435699ec4d39
Alex Gutierrez <algusaem@gmail.com>
2023-03-07 21:03:57 +0100

    AG - Get Users Node

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const mongoURI = 'mongodb+srv://algusaem:EcTjzxXVQ1fUEjmr@task-manager.wuasuef.mongodb.net/task-manager';
+const mongoOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+
+mongoose.connect(mongoURI, mongoOptions)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+  });
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Primer mensaje de la API');
+// Return the users.
+app.get('/users', (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((error) => {
+      res.status(500).send('Error retrieving users from database');
+    });
 });
 
-app.get('/listas/' + id, (req, res) => {
-  res.send('Devuelve las listas de tareas con la id' + id);
-});
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
